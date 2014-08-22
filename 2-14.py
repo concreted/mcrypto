f6807cdc499f1e3ab789b2b84c1a9e6431a43fe4
concreted <arichuang@gmail.com>
2014-08-22 14:27:54 -0700

    Reorganize folders

--- 2-14.py
+++
@@ -1,7 +0,0 @@
-# Matasano Crypto
-# 2-14
-
-from mclib import *
-
-#find_appended(consistent_encryption_oracle)
-find_appended_random(inconsistent_encryption_oracle)
--- test-1.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for i in range(8):
-	i = i + 1
-	print "==========1-%i==========" % i
-	os.system('python 1-%i.py' % i)
