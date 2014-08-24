e3d10d5b96b1846cc4cd973a3de67df7b3ad0ea7
concreted <arichuang@gmail.com>
2014-08-23 17:07:11 -0700

    18 - CTR mode fixes
    Use correct ctr format (8-byte nonce + 8-byte block count)
    Add IntToBytestring function to convert nonce/counter to byte string

--- pset3/18.py
+++ pset3/18.py
@@ -1,15 +1,33 @@
 from mclib import *
 
-def CTR(counter, key):
-    ctr = str(bytearray([0] * 8 + [counter] + [0] * 7))
+def IntToBytestring(val, num_bytes, endianness='big'):
+    val = '%x' % val
+    if len(val) % 2 != 0:
+        val = '0' + val
+
+    result = ba.unhexlify(val)
+
+    if endianness == 'little':
+        result = result[::-1]
+
+    result += '\x00' * (num_bytes - len(result))
+
+    return result
+
+def CTR(counter, key, nonce):
+    nonce_fmt = IntToBytestring(nonce, 8, 'little')
+    ctr_fmt = IntToBytestring(counter, 8, 'little')
+
+    ctr = nonce_fmt + ctr_fmt
+    
     return encrypt_ECB(ctr, key)
 
 def encrypt_CTR(plaintext, key, nonce=0):
     keystream = ''
-    
+    counter = 0
     while len(keystream) < len(plaintext):
-        keystream += CTR(nonce, key)
-        nonce += 1
+        keystream += CTR(counter, key, nonce)
+        counter += 1
         
     keystream = keystream[:len(plaintext)]
     
@@ -22,4 +40,3 @@ c = Base64ToASCII("L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0
 key = "YELLOW SUBMARINE"
 
 print ba.unhexlify(decrypt_CTR(c, key))
-
