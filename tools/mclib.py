5cb6e812836e3ba8450b5f05756880c5d264e52f
concreted <arichuang@gmail.com>
2014-08-23 20:44:06 -0700

    Redefine Base64ToHex using Base64ToASCII

--- tools/mclib.py
+++ tools/mclib.py
@@ -12,10 +12,10 @@ def hexToBase64(s):
 # Base64ToHex(string s)
 # Convert base 64 string to hex without leading/trailing whitespace.
 def Base64ToHex(s):
-	return ba.hexlify(ba.a2b_base64(s)).strip()
+	return ba.hexlify(Base64ToASCII(s))
 
 def Base64ToASCII(s):
-	return ba.unhexlify(Base64ToHex(s))
+	return ba.a2b_base64(s).strip()
 	
 # ASCIIToBinary(string s)
 # Convert ASCII string to binary (left-padded to 8 digits).
